96c5f2d0acad1acf5baac58ffd5c94f6a7bac391
lvsecoto <lvsecoto@gmail.com>
2024-03-14 16:55:38 +0800

    Update text_editor.dart
    remove unused import

--- lib/modules/text_editor.dart
+++ lib/modules/text_editor.dart
@@ -1,8 +1,6 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:pro_image_editor/utils/design_mode.dart';
 import 'package:rounded_background_text/rounded_background_text.dart';
 
