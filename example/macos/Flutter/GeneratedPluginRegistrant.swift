85f6aafd029d74460abda5409a2321a0c902f24c
hm21 <alex.frei@hotmail.ch>
2024-05-05 08:16:59 +0200

    fix: resolve platform integration issues for example application (closes #34)
    Address cross-platform compatibility issues in the example application by updating configuration files and adding necessary platform-specific files for Linux, macOS, and Windows. This fix ensures proper plugin registration and build configuration across all supported platforms. Modifications include updates to .gitignore, metadata, and various platform-specific build files and schemes.

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import emoji_picker_flutter
+import image_editor_common
+import path_provider_foundation
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  ImageEditorPlugin.register(with: registry.registrar(forPlugin: "ImageEditorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
