71a4d4f72c9809218014e0d520b7b0eb0403bdc0
Jona T. Feucht <14951074+jonafeucht@users.noreply.github.com>
2026-06-05 08:20:54 +0200

    Update Package.swift

--- darwin/pro_image_editor/Package.swift
+++ darwin/pro_image_editor/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "pro-image-editor", targets: ["pro_image_editor"])
     ],
      dependencies: [
-        .package(name: "FlutterFramework", path: "../FlutterFramework"),
+        .package(path: "../FlutterFramework"),
     ],
     targets: [
         .target(
