2161485bd69413539e562095e24c57b1833a2377
Jona T. Feucht <14951074+jonafeucht@users.noreply.github.com>
2026-06-06 08:38:38 +0200

    chore: formatting

--- darwin/pro_image_editor/Package.swift
+++ darwin/pro_image_editor/Package.swift
@@ -3,29 +3,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "pro_image_editor",
-    platforms: [
-        .iOS(.v12),
-        .macOS(.v11),
-    ],
-    products: [
-        .library(name: "pro-image-editor", targets: ["pro_image_editor"])
-    ],
-    dependencies: [
-        .package(path: "../FlutterFramework")
-    ],
-    targets: [
-        .target(
-            name: "pro_image_editor",
-            dependencies: [
-                .product(name: "FlutterFramework", package: "FlutterFramework")
-            ],
-            path: ".",
-            resources: [],
-            linkerSettings: [
-                .linkedFramework("Flutter", .when(platforms: [.iOS])),
-                .linkedFramework("FlutterMacOS", .when(platforms: [.macOS])),
-            ]
-        )
-    ]
+  name: "pro_image_editor",
+  platforms: [
+    .iOS(.v12),
+    .macOS(.v11),
+  ],
+  products: [
+    .library(name: "pro-image-editor", targets: ["pro_image_editor"])
+  ],
+  dependencies: [
+    .package(path: "../FlutterFramework")
+  ],
+  targets: [
+    .target(
+      name: "pro_image_editor",
+      dependencies: [
+        .product(name: "FlutterFramework", package: "FlutterFramework")
+      ],
+      path: ".",
+      resources: [],
+      linkerSettings: [
+        .linkedFramework("Flutter", .when(platforms: [.iOS])),
+        .linkedFramework("FlutterMacOS", .when(platforms: [.macOS])),
+      ]
+    )
+  ]
 )
